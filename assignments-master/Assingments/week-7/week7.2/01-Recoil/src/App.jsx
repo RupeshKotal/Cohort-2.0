0b0d420fba5ba18bab623c2f57efa1cfbc36aa3c
RupeshKotal <rupeshkotal99@gmail.com>
2024-01-19 00:49:00 +0530

    recoil

--- assignments-master/Assingments/week-7/week7.2/01-Recoil/src/App.jsx
+++ assignments-master/Assingments/week-7/week7.2/01-Recoil/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import { countAtom } from "./store/Atoms/count";
-import {RecoilRoot,useRecoilState,useRecoilValue}  from 'recoil'
+import {RecoilRoot,useRecoilState,useRecoilValue, useSetRecoilState}  from 'recoil'
+import {evenSelector} from "./store/Atoms/count"
+
 
 function App() {
 
@@ -15,10 +17,13 @@ function App() {
 }
 
 function Count() {
+
+  console.log("Count Rendewr");
   return (
     <div>
       <CountRender></CountRender>
       <Button ></Button>
+      <EvenRender></EvenRender>
     </div>
   );
 }
@@ -33,29 +38,57 @@ function CountRender() {
 
 
 //Button Componnet
-function Button() {
-  const [count,setCount]= useRecoilState(countAtom);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increase
-      </button>
 
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Decrease
-      </button>
+//*** In this button component also re-render */
+// function Button() {
+//   const [count,setCount]= useRecoilState(countAtom);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}
+//       >
+//         Increase
+//       </button>
+
+//       <button
+//         onClick={() => {
+//           setCount(count - 1);
+//         }}
+//       >
+//         Decrease
+//       </button>
+//     </div>
+//   );
+// }
+
+function Button(){
+ const setCount = useSetRecoilState(countAtom);
+ console.log("button rendered");
+  return(
+    <div>
+      <button onClick={()=>{
+        setCount((e)=>(
+          e+1
+        ))
+      }}>Increase</button>
+
+      <button onClick={()=>{
+        setCount((e)=>(
+          e-1
+        ))
+      }}>Decrase</button>
     </div>
-  );
+  )
 }
 
+function EvenRender(){
+ const count = useRecoilValue(evenSelector);
 
+return <div>
+  {count ? "It is Even": "It is ODd"}
+</div>
+}
 
 export default App;
